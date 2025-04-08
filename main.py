0a67605891dfdc4ceceab6b31adb37cae69898e1
adamjames870 <adamjames870@yahoo.co.uk>
2025-04-08 16:09:56 +0800

    Limited FPS

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from constants import *
 
 def main():
     pygame.init
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         for event in pygame.event.get():
@@ -11,6 +14,7 @@ def main():
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
     
 if __name__ == "__main__":
     main()
